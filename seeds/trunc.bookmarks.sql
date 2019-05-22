41ee9912c8f41960b1c5452681333376244d3226
Drew Love <davidandrewlove84@gmail.com>
2019-05-22 11:22:44 -0400

    Update trunc.bookmarks.sql

--- seeds/trunc.bookmarks.sql
+++ seeds/trunc.bookmarks.sql
@@ -1,16 +1,1 @@
-TRUNCATE bookmarks; 
-
-serializeBookmarks = bookmark => ({
-    title: bookmark.title,
-})
-
-router
-.route('/bookmarks')
-.get((req, res, next) => {
-    bookmarksService.getAllBookmarks(req.app.get('app'))
-    .status(400)
-    .then(bookmarks => {
-        res.json(bookmarks.map(serializeBookmarks))
-    })
-    .catch(next)
-})
+TRUNCATE TABLE bookmarks; 
